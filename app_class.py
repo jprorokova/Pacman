81053f9a5996bb050ddbd296519ace0446ff1817
Julia <j.prorokova@gmail.com>
2021-09-07 19:41:33 +0300

    added player class

--- app_class.py
+++ app_class.py
@@ -1,8 +1,9 @@
 import pygame,sys
 from settings import *
-
+from player_class import *
 pygame.init()
 vec = pygame.math.Vector2
+
 
 
 class App:
@@ -10,22 +11,77 @@ class App:
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.state = "intro"
+        self.state = "start"
+        self.cell_width = MAZE_WIDTH//28
+        self.cell_height = MAZE_HEIGHT // 30
+        self.player = Player(self,PLAYER_START_POS)
+
+        self.load()
     def run(self):
         while self.running:
-            if self.state =="intro":
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state =="start":
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
-########################### INTRO FUNCTIONS ###################################
-    def intro_events(self):
+########################### HELPer FUNCTIONS ###################################
+    def draw_text(self,words,screen,pos,size,colour,font_name,centered = False):
+        font = pygame.font.SysFont(font_name,size)
+        text = font.render(words,False,colour)
+        text_size = text.get_size()
+        if centered:
+            pos[0] = pos[0] - text_size[0]//2
+            pos[1] = pos[1] - text_size[1]//2
+        screen.blit(text,pos)
+
+    def load(self):
+        self.background = pygame.image.load('maze.png')
+        self.background = pygame.transform.scale(self.background,(MAZE_WIDTH,MAZE_HEIGHT))
+    def draw_grid(self):
+        for x in range(WIDTH//self.cell_width):
+            pygame.draw.line(self.background,GREY,(x*self.cell_width,0),(x*self.cell_width,HEIGHT))
+        for x in range(HEIGHT//self.cell_height):
+            pygame.draw.line(self.background,GREY,(0,x*self.cell_height),(WIDTH,x*self.cell_height))
+########################### START FUNCTIONS ###################################
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-    def intro_update(self):
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = "playing"
+    def start_update(self):
         pass
-    def intro_draw(self):
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('PUSH SPACE BAR',self.screen,[WIDTH//2,HEIGHT//2],START_TEXT_SIZE, (239,193,41),START_FONT,True)
+        self.draw_text('1 PLAYER ONLY', self.screen, [WIDTH // 2, HEIGHT // 2+50], START_TEXT_SIZE, (50,173,242),
+                       START_FONT,True)
+        self.draw_text('HIGH SCORE', self.screen, [4,0], START_TEXT_SIZE, (255, 255, 255),
+                       START_FONT)
+        pygame.display.update()
+
+
+########################### PLAYING FUNCTIONS ###################################
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def playing_update(self):
+        pass
+    def playing_draw(self):
+        self.screen.fill(BLACK)
+        self.screen.blit(self.background,(TOP_BOTTOM_BUFFER//2,TOP_BOTTOM_BUFFER//2))
+        self.draw_grid()
+        self.draw_text('CURRENT SCORE: 0',self.screen,[50,0],18,WHITE,START_FONT,centered = False)
+        self.draw_text('HIGH SCORE: 0', self.screen, [WIDTH//2+50, 0], 18, WHITE, START_FONT, centered=False)
+        self.player.draw()
         pygame.display.update()
--- settings.py
+++ settings.py
@@ -1,6 +1,21 @@
+from pygame.math import Vector2 as vec
+
 #SCREEN SETTINGS
-WIDTH,HEIGHT = 500,700
+TOP_BOTTOM_BUFFER = 50
+WIDTH,HEIGHT = 610,670
+MAZE_WIDTH,MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER,HEIGHT-TOP_BOTTOM_BUFFER
 FPS = 60
+
 #colour settings
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREY = (120,120,120)
+WHITE = (255,255,255)
+PLAYER_COLOUR = (190,194,15)
+#font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
 #player settings
+PLAYER_START_POS = vec(1,10)
+
 #mob settings
