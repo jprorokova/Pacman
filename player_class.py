10e9f838086352928babec6dad46f7b67722d2c7
Julia <j.prorokova@gmail.com>
2021-10-01 10:46:32 +0300

    added algorithm generation

--- player_class.py
+++ player_class.py
@@ -1,18 +1,111 @@
-from settings import *
 import pygame
+from settings import *
 vec = pygame.math.Vector2
+
+
 class Player:
-    def __init__(self,app,pos):
+    def __init__(self, app, pos):
         self.app = app
+        self.starting_pos = [pos.x, pos.y]
         self.grid_pos = pos
-        self.pix_pos = vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+
-                           self.app.cell_width//2,
-                           (self.grid_pos.y*self.app.cell_height)+TOP_BOTTOM_BUFFER//2+
-                           self.app.cell_height//2)
-        print(self.grid_pos,self.pix_pos)
+        self.pix_pos = self.get_pix_pos()
+        self.direction = vec(1, 0)
+        self.stored_direction = None
+        self.able_to_move = True
+        self.current_score = 0
+        self.speed = 2
+        self.lives = 1
+
+    # def bfs_shortest_path(self, start, goal):
+    #
+    #     grid = [[0 for x in range(28)] for x in range(30)]
+    #     for cell in self.app.walls:
+    #         if cell.x < 28 and cell.y < 30:
+    #             grid[int(cell.y)][int(cell.x)] = 1
+    #     explored = []
+    #
+    #     queue = [(start,[start])]
+    #
+    #
+    #     if start == goal:
+    #         return "That was easy! Start = goal"
+    #
+    #
+    #     while queue:
+    #
+    #         (current,path) = queue.pop(0)
+    #
+    #         explored.append(current)
+    #         neighbours = [[int(current[0]+1),int(current[1])],
+    #                       [int(current[0]-1),int(current[1])],
+    #                       [int(current[0]),int(current[1]+1)],
+    #                       [int(current[0]),int(current[1]-1)]]
+    #
+
 
     def update(self):
-        pass
+        if self.able_to_move:
+            self.pix_pos += self.direction*self.speed
+        if self.time_to_move():
+            if self.stored_direction != None:
+                self.direction = self.stored_direction
+            self.able_to_move = self.can_move()
+        # Setting grid position in reference to pix pos
+        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER +
+                            self.app.cell_width//2)//self.app.cell_width+1
+        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER +
+                            self.app.cell_height//2)//self.app.cell_height+1
+        # alg 
+
+        if self.on_coin():
+            self.eat_coin()
+
     def draw(self):
-        pygame.draw.circle(self.app.screen,PLAYER_COLOUR,
-                           self.pix_pos,self.app.cell_width//2-2)
+        pygame.draw.circle(self.app.screen, PLAYER_COLOUR, (int(self.pix_pos.x),
+                                                            int(self.pix_pos.y)), self.app.cell_width//2-2)
+
+        # Drawing player lives
+        for x in range(self.lives):
+            pygame.draw.circle(self.app.screen, PLAYER_COLOUR, (30 + 20*x, HEIGHT - 15), 7)
+
+        # Drawing the grid pos rect
+        # pygame.draw.rect(self.app.screen, RED, (self.grid_pos[0]*self.app.cell_width+TOP_BOTTOM_BUFFER//2,
+        #                                         self.grid_pos[1]*self.app.cell_height+TOP_BOTTOM_BUFFER//2, self.app.cell_width, self.app.cell_height), 1)
+
+    def on_coin(self):
+        if self.grid_pos in self.app.coins:
+            if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+                if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                    return True
+            if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+                if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                    return True
+        return False
+
+    def eat_coin(self):
+        self.app.coins.remove(self.grid_pos)
+        self.current_score += 1
+
+    def move(self, direction):
+        self.stored_direction = direction
+
+    def get_pix_pos(self):
+        return vec((self.grid_pos[0]*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+self.app.cell_width//2,
+                   (self.grid_pos[1]*self.app.cell_height) +
+                   TOP_BOTTOM_BUFFER//2+self.app.cell_height//2)
+
+        print(self.grid_pos, self.pix_pos)
+
+    def time_to_move(self):
+        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
+                return True
+        if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
+                return True
+
+    def can_move(self):
+        for wall in self.app.walls:
+            if vec(self.grid_pos+self.direction) == wall:
+                return False
+        return True
